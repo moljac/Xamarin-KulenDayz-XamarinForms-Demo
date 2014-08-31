bca9dfc74cd24b9e35d36240d1c010d56472f979
moljac <mcvjetko@holisticware.net>
2014-08-31 19:48:16 +0200

    xaml samples for Pages

---
+++ XamarinForms.Demo.Pages.PCL/XamarinForms.Demo.Pages.PCL/ClaNavigationPageDemo.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XamarinForms.Demo.Pages.PCL
+{
+	public partial class ClaNavigationPageDemo
+	{
+		public ClaNavigationPageDemo()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ XamarinForms.Demo.Pages.PCL/XamarinForms.Demo.Pages.PCL/MasterDetail2.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace My.Shared
+{
+	public partial class MasterDetailPageTest : MasterDetailPage
+	{
+		public MasterDetailPageTest()
+		{
+			InitializeComponent();
+		}
+	}
+}
